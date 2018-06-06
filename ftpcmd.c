bf64fa503f4ffde7ff75757bdbc912d2d6e838c3
Joachim Nilsson <troglobit@gmail.com>
2018-06-06 14:35:28 +0200

    Add support for changing file modification time with MDTM
    RFC 3659 explicitly lists "MDTM 20180606143600 /path/to/file" as not
    supported, but many clients send this after a STOR command.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- ftpcmd.c
+++ ftpcmd.c
@@ -1062,10 +1062,19 @@ static void handle_RETR(ctrl_t *ctrl, char *file)
 
 static void handle_MDTM(ctrl_t *ctrl, char *file)
 {
-	char buf[80];
-	char *path;
-	struct tm *tm;
 	struct stat st;
+	struct tm *tm;
+	char *path, *ptr;
+	char *mtime = NULL;
+	char buf[80];
+
+	/* Request to set mtime, ncftp does this */
+	ptr = strchr(file, ' ');
+	if (ptr) {
+		*ptr++ = 0;
+		mtime = file;
+		file  = ptr;
+	}
 
 	path = compose_abspath(ctrl, file);
 	if (!path || stat(path, &st) || !S_ISREG(st.st_mode)) {
@@ -1073,8 +1082,32 @@ static void handle_MDTM(ctrl_t *ctrl, char *file)
 		return;
 	}
 
+	if (mtime) {
+		struct timespec times[2] = {
+			{ 0, UTIME_OMIT },
+			{ 0, 0 }
+		};
+		struct tm tm;
+		int rc;
+
+		if (!strptime(mtime, "%Y%m%d%H%M%S", &tm)) {
+		fail:
+			send_msg(ctrl->sd, "550 Invalid time format\r\n");
+			return;
+		}
+
+		times[1].tv_sec = mktime(&tm);
+		rc = utimensat(0, path, times, 0);
+		if (rc) {
+			ERR(errno, "Failed setting MTIME %s of %s", mtime, file);
+			goto fail;
+		}
+		stat(path, &st);
+	}
+
 	tm = gmtime(&st.st_mtime);
 	strftime(buf, sizeof(buf), "213 %Y%m%d%H%M%S\r\n", tm);
+
 	send_msg(ctrl->sd, buf);
 }
 

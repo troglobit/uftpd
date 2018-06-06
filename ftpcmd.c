ef37902a465e98e1a597e8712a0af082dde0bd5d
Joachim Nilsson <troglobit@gmail.com>
2018-06-06 16:18:10 +0200

    Add support for FTP DELE command
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- ftpcmd.c
+++ ftpcmd.c
@@ -1230,11 +1230,30 @@ static void handle_STOR(ctrl_t *ctrl, char *file)
 	do_PORT(ctrl, 3);
 }
 
-#if 0
 static void handle_DELE(ctrl_t *ctrl, char *file)
 {
+	char *path;
+
+	path = compose_abspath(ctrl, file);
+	if (!path) {
+		ERR(errno, "Cannot find %s", file);
+		goto fail;
+	}
+
+	if (remove(path)) {
+		if (ENOENT == errno)
+		fail:	send_msg(ctrl->sd, "550 No such file or directory.\r\n");
+		else if (EPERM == errno)
+			send_msg(ctrl->sd, "550 Not allowed to remove file.\r\n");
+		else
+			send_msg(ctrl->sd, "550 Unknown error.\r\n");
+		return;
+	}
+
+	send_msg(ctrl->sd, "200 Command OK\r\n");
 }
 
+#if 0
 static void handle_MKD(ctrl_t *ctrl, char *arg)
 {
 }
@@ -1415,6 +1434,7 @@ static void handle_UNKNOWN(ctrl_t *ctrl, char *command)
 
 static ftp_cmd_t supported[] = {
 	COMMAND(ABOR),
+	COMMAND(DELE),
 	COMMAND(USER),
 	COMMAND(PASS),
 	COMMAND(SYST),

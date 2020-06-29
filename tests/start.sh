2af596dc65faa99afd07d76e9047a72cf91d7845
Joachim Nilsson <troglobit@gmail.com>
2020-06-30 00:57:39 +0200

    Chanage custom TFTP port for tftp unit tests
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- tests/start.sh
+++ tests/start.sh
@@ -2,7 +2,7 @@
 cd ../tests
 cp /etc/passwd testfile.txt
 
-../src/uftpd -n -o ftp=9013,tftp=9014 -l none . &
+../src/uftpd -n -o ftp=9013,tftp=6969 -l none . &
 echo $! >/tmp/uftpd.pid
 
 sleep 1
--- tests/tftp.sh
+++ tests/tftp.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+set -x
 cd /tmp
-tftp -4 127.0.0.1 9014 -m binary -c get testfile.txt
+tftp -4 127.0.0.1 6969 -m binary -c get testfile.txt

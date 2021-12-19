d752c34b26aec4b45f39ec82209959afb99e4963
Joachim Wiberg <troglobit@gmail.com>
2021-12-19 11:30:06 +0100

    Minor, clarify intent with parenthesis (clang-tidy)
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/ftpcmd.c
+++ src/ftpcmd.c
@@ -476,16 +476,16 @@ static char *mode_to_str(mode_t m)
 	static char str[11];
 
 	snprintf(str, sizeof(str), "%c%c%c%c%c%c%c%c%c%c",
-		 S_ISDIR(m)  ? 'd' : '-',
-		 m & S_IRUSR ? 'r' : '-',
-		 m & S_IWUSR ? 'w' : '-',
-		 m & S_IXUSR ? 'x' : '-',
-		 m & S_IRGRP ? 'r' : '-',
-		 m & S_IWGRP ? 'w' : '-',
-		 m & S_IXGRP ? 'x' : '-',
-		 m & S_IROTH ? 'r' : '-',
-		 m & S_IWOTH ? 'w' : '-',
-		 m & S_IXOTH ? 'x' : '-');
+		 S_ISDIR(m)    ? 'd' : '-',
+		 (m & S_IRUSR) ? 'r' : '-',
+		 (m & S_IWUSR) ? 'w' : '-',
+		 (m & S_IXUSR) ? 'x' : '-',
+		 (m & S_IRGRP) ? 'r' : '-',
+		 (m & S_IWGRP) ? 'w' : '-',
+		 (m & S_IXGRP) ? 'x' : '-',
+		 (m & S_IROTH) ? 'r' : '-',
+		 (m & S_IWOTH) ? 'w' : '-',
+		 (m & S_IXOTH) ? 'x' : '-');
 
 	return str;
 }
